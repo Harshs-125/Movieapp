63103e479f02411577ef314f1814938856db0d6a
Harsh <soni.hs661@gmail.comm>
2021-10-11 22:49:06 +0530

    css and ui

---
+++ src/components/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Navbar from './Navbar';
+import MovieCard  from './MovieCard';
+import { data } from '../data';
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <div className="main">
+        <div className="tabs">
+          <div className="tab">Movie</div>
+          <div className="tab">Favourites</div>
+        </div>
+        <div className="list">
+          {data.map(movie=>(
+            <MovieCard movie={movie}/>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
