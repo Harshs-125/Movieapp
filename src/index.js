f939d4e5bc69cab9178cb21ba66ba5c92ff411f2
Harsh <soni.hs661@gmail.comm>
2021-10-13 19:03:25 +0530

    modifie middleware in index.js

--- src/index.js
+++ src/index.js
@@ -5,16 +5,22 @@ import { createStore , applyMiddleware} from 'redux';
 import './index.css';
 import App from './components/App';
 import rootReducer from './reducers/index';
+import { ConnectionPoolClosedEvent } from 'mongodb';
 
 //function logger(obj,next,action)
-const logger=function({dispatch,getState}){
-  return function(next){
-    return function(action){
-      //Middleware Code
-      console.log("Action-type =",action.type);
-      next(action);
-    }
-  }
+// const logger=function({dispatch,getState}){
+//   return function(next){
+//     return function(action){
+//       //Middleware Code
+//       console.log("Action-type =",action.type);
+//       next(action);
+//     }
+//   }
+// }
+//modified middleWare
+const logger= ({dispatch,getState})=> (next)=> (action)=>{
+  console.log("Action-type =",action.type);
+  next(action);
 }
 const store=createStore(rootReducer,applyMiddleware(logger));
 console.log("before action-",store.getState());
